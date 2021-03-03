b6d87626f4ae8d6f10dbf6af39a8b3eb317bdca8
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-03-03 12:11:19 +0100

    aded main instance for the onsu compiler

---
+++ sources/main/java/onsu/tools/compiler/OnsuCompiler.java
@@ -0,0 +1,12 @@
+package onsu.tools.compiler;
+
+/**
+ * Import other utils in here
+ */
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
