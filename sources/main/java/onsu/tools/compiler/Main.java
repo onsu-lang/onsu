905553216bbfa4189e87ce7ded3f9aaae3cc703e
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-03-03 12:10:13 +0100

    added main file for compiler args

---
+++ sources/main/java/onsu/tools/compiler/Main.java
@@ -0,0 +1,7 @@
+package onsu.tools.compiler;
+
+public final class Main {
+	public static void main(String[] args) {
+		System.exit(new OnsuCompiler().run(System.in, System.out, System.err, args));
+	}
+}
